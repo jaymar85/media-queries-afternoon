b6d4d8b1cc811b9aa91c61b37098b353e9248486
Jeremy Mark <jeremymark@Christinas-MBP.5-A-Rm509>
2019-09-17 00:20:39 -0500

    finished project

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// import "../styles/Header.css";
+
+// class Header extends React.Component {
+//     render() {
+//         return(
+//             <main className="container">
+//                 <div className="welcome">
+//                     <h2>Welcome To Our Studio!</h2>     
+//                 </div>
+
+//                 <div className="introduce">
+//                     <h1>IT'S NICE TO MEET YOU</h1>
+//                 </div> 
+//                 <div className="button">
+//                     <button><p>TELL ME MORE</p></button>
+//                 </div>
+//             </main>
+//         )
+//     }
+// }
+
+// export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "../styles/Nav.css";
+
+class Nav extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {menuOpenStatus: "side-menu"}    
+    }
+
+   toggle = () => {
+        if(this.state.menuOpenStatus === "side-menu-close" || this.state.menuOpenStatus === "side-menu") {
+            this.setState({menuOpenStatus: "side-menu-open"});
+        } else if(this.state.menuOpenStatus === "side-menu-open") {
+            this.setState({menuOpenStatus: "side-menu-close"});
+        }
+    }
+
+    render() {
+        // const {menuOpenStatus} = this.state; // line 42 No this.state
+        return (
+            <>
+                <nav>
+                    <div>
+                        <h1>Start Bootstrap</h1>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>SERVICES</li>
+                            <li>PORTFOLIO</li>
+                            <li>ABOUT</li>
+                            <li>TEAM</li>
+                            <li>CONTACT</li>
+                            <button className="hamburger" onClick={this.toggle}>MENU
+                            <span >&#9776;</span>   
+                            </button>
+                        </ul>
+                    </div>
+                </nav>
+                <div className={`${this.state.menuOpenStatus}`}>
+                    <h4>SERVICES</h4>
+                    <h4>PORTFOLIO</h4>
+                    <h4>ABOUT</h4>
+                    <h4>TEAM</h4>
+                    <h4>CONTACT</h4>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Nav;
+
+/* <img  */
+// src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png" alt="hamburger"/>
